3def0e8ef1da1ad46923453f377c3e979a39ff33
Listwas <listwa.patrykkr@gmail.com>
2025-10-05 15:48:35 +0200

    feat: add /booklist endpoint

--- booker-backend/main.py
+++ booker-backend/main.py
@@ -10,3 +10,8 @@ async def root():
 @app.get("/ping")
 async def ping():
     return {"message": "Hello"}
+
+# you can view someone's list but only owner can edit
+@app.get("/booklist")
+async def booklist():
+    return {"message": "booklist"}

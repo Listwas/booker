efb048deb796898fb42641991a0a148b675aeec6
Listwas <listwa.patrykkr@gmail.com>
2025-10-06 19:07:51 +0200

    feat: drop down menu preparation

---
+++ booker-front/src/pages/Home/nav/drop-down-menu/DropDownMenu.tsx
@@ -0,0 +1,13 @@
+
+
+function DropDownMenu() {
+    return (
+        <>
+            <div>
+                drop down menu will be here
+            </div>    
+        </>
+    )
+}
+
+export default DropDownMenu;

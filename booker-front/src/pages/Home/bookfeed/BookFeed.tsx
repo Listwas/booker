841e89f508569fa898149702d75575b7bea5b8bb
Listwas <listwa.patrykkr@gmail.com>
2025-10-06 19:06:34 +0200

    feat: book feed preparation

---
+++ booker-front/src/pages/Home/bookfeed/BookFeed.tsx
@@ -0,0 +1,13 @@
+
+
+function BookFeed() {
+    return (
+        <>
+            <div>
+                book feed will be here someday
+            </div>
+        </>
+    )
+}
+
+export default BookFeed;

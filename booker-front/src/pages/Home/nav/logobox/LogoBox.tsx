738a2adfc91c0fd1db0f0645378dd7ccc05b4542
Listwas <listwa.patrykkr@gmail.com>
2025-10-07 15:09:38 +0200

    refactor: logo link styling

--- booker-front/src/pages/Home/nav/logobox/LogoBox.tsx
+++ booker-front/src/pages/Home/nav/logobox/LogoBox.tsx
@@ -1,13 +1,23 @@
-
+import React from 'react';
+import constants from '../../constants.json'
 
 function LogoBox() {
+    const logo_style: React.CSSProperties = {
+        color: '#9400d3',
+        marginLeft: '12px',
+        fontSize: '2rem',
+        fontWeight: 800,
+        textDecoration: 'none',
+        cursor: 'pointer',
+    };
+
     return (
         <>
             <div>
-                logo box
+                <a href={constants.HOME_PAGE_LINK} style={logo_style}>Booker</a>
             </div>
         </>
-    )
+    );
 }
 
 export default LogoBox;

ae9e6766e50e1f37adb3272bda3a27bf4cba8c25
Listwas <listwa.patrykkr@gmail.com>
2025-10-12 10:40:24 +0200

    refactor: search svg

--- booker-front/src/pages/Home/nav/search/Search.tsx
+++ booker-front/src/pages/Home/nav/search/Search.tsx
@@ -1,9 +1,34 @@
+import styles from './search.module.css'
 
 function Search() {
     return (
         <>
-            <div>
-                <p> search bar </p>
+            <div className={styles.search_container}>
+                <input className={styles.search} type="text" placeholder="type here" />
+
+                <svg 
+                    width="26" 
+                    height="26" 
+                    viewBox="0 0 26 26" 
+                    fill="none" 
+                    xmlns="http://www.w3.org/2000/svg">
+
+                <circle 
+                    cx="10" 
+                    cy="10" 
+                    r="9" 
+                    stroke="black" 
+                    stroke-width="2"/>
+
+                <line 
+                    x1="17.1213" 
+                    y1="17" 
+                    x2="24" 
+                    y2="23.8787" 
+                    stroke="black" 
+                    stroke-width="3" 
+                    stroke-linecap="round"/>
+                </svg>
             </div>
         </>
     )

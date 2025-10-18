c126c50d05b39e5ba54e24f34213ad831e505289
Listwas <listwa.patrykkr@gmail.com>
2025-10-18 15:01:35 +0200

    feat: read books from local JSON file

--- booker-front/src/pages/Home/bookfeed/BookFeed.tsx
+++ booker-front/src/pages/Home/bookfeed/BookFeed.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+import booksData from "./books.json";
 
 
 function BookFeed() {
-    return (
-        <>
-            <div>
-                book feed will be here someday
-            </div>
-        </>
-    )
+  const [books, setBooks] = useState(booksData);
+
+  return (
+    <div>
+      <h2>Book Feed</h2>
+      {books.map((books, index) => (
+        <div key={index}>
+          <strong>{books.title}</strong> — {books.author}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default BookFeed;

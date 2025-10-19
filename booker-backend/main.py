cf1e90d4cf78c887860c6adf24e7d63b64900118
Listwas <listwa.patrykkr@gmail.com>
2025-10-19 16:15:46 +0200

    feat: display books by openlibrary api call

--- booker-backend/main.py
+++ booker-backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from constants import DATABASE_URL
 from fastapi.middleware.cors import CORSMiddleware
+import requests
 
 app = FastAPI()
 
@@ -12,14 +13,17 @@ app.add_middleware(
 )
 
 @app.get("/books")
-def get_books():
-    return [
-        {"title": "The Hobbit", "author": "J.R.R. Tolkien"},
-        {"title": "Dune", "author": "Frank Herbert"},
-        {"title": "1984", "author": "George Orwell"},
-        {"title": "1984", "author": "George Orwell"}
-    ]
-
+def get_books(query: str = "python"):
+    res = requests.get("https://openlibrary.org/search.json?q=python")
+    data = res.json()
+    
+    books = []
+    for book in data["docs"][:2]:
+        books.append({
+            "title": book["title"],
+            "author": ", ".join(book.get("author_name", []))
+        })
+    return books
 
 # you can view someone's list but only owner can edit
 @app.get("/booklist")
--- booker-front/src/pages/Home/bookfeed/BookFeed.tsx
+++ booker-front/src/pages/Home/bookfeed/BookFeed.tsx
@@ -6,7 +6,10 @@ function BookFeed() {
     useEffect(() => {
         fetch("http://localhost:8000/books")
             .then(res => res.json())
-            .then(data => setBooks(data));
+            .then(data => {
+                    console.log(data);
+                    setBooks(data);
+            });
     }, []);
 
   return (

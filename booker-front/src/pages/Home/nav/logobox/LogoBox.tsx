23130d3e317a13df1368bad8dbdff8e66052b2cb
Listwas <listwa.patrykkr@gmail.com>
2025-10-20 13:46:32 +0200

    refactor: major NavBar styling

--- booker-front/src/pages/Home/nav/logobox/LogoBox.tsx
+++ booker-front/src/pages/Home/nav/logobox/LogoBox.tsx
@@ -1,20 +1,10 @@
-import React from 'react';
 import constants from '../../constants.json'
 
 function LogoBox() {
-    const logo_style: React.CSSProperties = {
-        color: '#9400d3',
-        marginLeft: '12px',
-        fontSize: '2rem',
-        fontWeight: 800,
-        textDecoration: 'none',
-        cursor: 'pointer',
-    };
-
     return (
         <>
             <div>
-                <a href={constants.HOME_PAGE_LINK} style={logo_style}>Booker</a>
+                <a href={constants.HOME_PAGE_LINK}>Booker</a>
             </div>
         </>
     );

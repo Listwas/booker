de167b487cc611de8e9a0ea4f9e8198ea73a0d06
Listwas <listwa.patrykkr@gmail.com>
2025-10-29 16:24:36 +0100

    feat: book list page

---
+++ booker-front/src/pages/booklist/BookList.tsx
@@ -0,0 +1,14 @@
+import s from './booklist.module.css';
+
+function BookList() {
+    return (
+        <>
+            <div className={s.vorp}> asda </div>
+        
+
+        </>
+    );
+
+}
+
+export default BookList;
